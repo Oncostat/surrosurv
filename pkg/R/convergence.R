a71d862350ab3303ed332c3fd96589ca276ce5bd
federicorotolo <federicorotolo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-28 15:23:02 +0000

    convergence with scaled gradient
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/surrosurv@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/convergence.R
+++ pkg/R/convergence.R
@@ -11,17 +11,18 @@ convals <- function(x) {
   copulas <- function(cop) {
     f <- function() {
       res <- t(sapply(x[[cop]], function(y) {
-        vals <- try({
-          c(maxAgrad = max(abs(attr(y$optimxRES, 'grad'))),
-            minev1 = tryCatch(
-              min(eigen(attr(y$optimxRES, 'hessian'))$values),
-              error = function(x) return(NA)))
-        }, silent = TRUE)
-        if (class(vals) == 'try-error') vals <- c(maxAgrad = NA, minev1 = NA)
-        return(rbind(c(vals,
-                       minev2 = tryCatch(
-                         min(eigen(y$VarCor2)$values),
-                         error=function(x) return(NA)))))
+        vals <- c(
+          maxSgrad = tryCatch(
+            max(abs(with(attributes(y$optimxRES), solve(hessian, grad)))),
+            error = function(x) return(Inf)),
+          minHev = tryCatch(
+            min(eigen(attr(y$optimxRES, 'hessian'))$values),
+            error = function(x) return(-1)),
+          minREev = tryCatch(
+            min(eigen(y$VarCor2)$values),
+            error=function(x) return(-1))
+        )
+        return(vals)
       }))
       rownames(res) <- paste(cop, rownames(res))
       return(res)
@@ -36,23 +37,27 @@ convals <- function(x) {
     convres <- t(sapply(
       which_models,
       function (y) tryCatch({
-        vals <- try({
-          c(maxAgrad = max(abs(y$optinfo$derivs$gradient)), 
-            minev1 = tryCatch(
-              min(eigen(y$optinfo$derivs$Hessian)$values),
-              error = function(x) return(NA)))
-        })
-        if (class(vals) == 'try-error') vals <- c(maxAgrad = NA, minev1 = NA)
-        return(c(vals,
-                 minev2 = tryCatch(min(eigen(y$VarCor$trialref)$values),
-                                   error = function(x) return(NA))))
-      }, error=function(x) return(rep(NA, 3))))
+        vals <- c(
+          maxSgrad = tryCatch(
+            max(abs(with(y$optinfo$derivs,solve(Hessian, gradient)))),
+            error = function(x) return(Inf)),
+          minHev = tryCatch(
+            min(eigen(y$optinfo$derivs$Hessian)$values),
+            error = function(x) return(-1)),
+          minREev = tryCatch(
+            min(eigen(y$VarCor$trialref)$values),
+            error = function(x) return(-1))
+        )
+        return(vals)
+      }, error=function(x) return(c(Inf, -1, -1))))
     )
     names(convres) <- names(which_models)
     return(convres)
   }
   
   fitRES <- do.call(rbind, lapply(models, function(x) eval(call(paste(x)))))
+  fitRES[grepl('unadj|PoissonI', rownames(fitRES)), 'minREev'] <- NA
+  class(fitRES) <- 'conv'
   return(fitRES)
 }
 
@@ -66,8 +71,18 @@ convergence.surrosurv <- function(x, kkttol = 1e-3, kkt2tol = 1e-6) {
   
   ConvRES <- convals(x)
   checkConvRES <- cbind(
-    ConvRES[, 1] <= kkttol,
+    ConvRES[, 1, drop=FALSE] <= kkttol,
     ConvRES[, 2:3] >= kkt2tol)
   
+  class(checkConvRES) <- 'conv'
   return(checkConvRES)
+}
+
+#####################################################################################
+#####################################################################################
+#####################################################################################
+
+print.conv <- function(x, na.print = '---', ...){
+  class(x) <- 'matrix'
+  print(x, na.print = na.print, ... )
 }
